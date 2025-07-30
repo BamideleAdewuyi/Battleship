8baf31225f1311e7476ffc2d74c9f2a2c3b72709
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-07-30 17:07:01 +0100

    ship.js: Removed sunk property from class

--- src/ship.js
+++ src/ship.js
@@ -1,7 +1,6 @@
 class Ship {
-    constructor(length, sunk = false, hits = 0) {
+    constructor(length, hits = 0) {
         this.length = length;
-        this.sunk = sunk;
         this.hits = hits;
     }
 
