980cba3230218cc3dbbe4d553c4d8ac4736d4da4
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-07-30 16:46:54 +0100

    ship.test.js: Wrote test that checks ship is sunk after 5 hits

--- src/ship.test.js
+++ src/ship.test.js
@@ -10,3 +10,12 @@ test('Ship class can register when its been hit', () => {
     ship.hit();
     expect(ship.hits).toBe(1);
 });
+
+test('Ship is sunk after five hits', () => {
+    ship.hit();
+    ship.hit();
+    ship.hit();
+    ship.hit();
+    ship.hit();
+    expect(ship.isSunk).toBe(true);
+})
