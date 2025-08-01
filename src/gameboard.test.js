152176eb6c24f813d39531cdf208b99e4765f692
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-08-01 21:37:04 +0100

    gameboard.test.js: Wrote test that checks checkSquareValid method returns false when square next to ship is passed

--- src/gameboard.test.js
+++ src/gameboard.test.js
@@ -16,6 +16,11 @@ test('Can place a ship of length 2, horizontally  at 0,1', () => {
     expect(gameboard.board[0].position).toStrictEqual([[0,1], [1,1]])
 });
 
+test('checkSquareValid method returns false with square next to placed ship', () => {
+    gameboard.placeShip(2, 'horizontal', 4, 4);
+    expect(gameboard.checkSquareValid(4, 3)).toBe(false)
+})
+
 test('Gameboard will not add a ship if any part of it is next to a ship already placed', () => {
     gameboard.placeShip(2, "horizontal", 4, 4);
 });
