e77d9faf09fe9f72c2009b43e86012b50c9e5ece
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-08-01 19:31:31 +0100

    gameboard.test.js: Created test for checking gameboard will not add a ship if it is positioned next to a ship already placed

--- src/gameboard.test.js
+++ src/gameboard.test.js
@@ -4,14 +4,18 @@ let gameboard;
 
 beforeEach(() => {
     gameboard = new Gameboard();
-})
+});
 
 test('Can place a ship of length 2, horizontally  at 0,0', () => {
     gameboard.placeShip(2, "horizontal", 0, 0);
     expect(gameboard.board[0].position).toStrictEqual([[0,0], [1,0]])
-})
+});
 
 test('Can place a ship of length 2, horizontally  at 0,1', () => {
     gameboard.placeShip(2, "horizontal", 0, 1);
     expect(gameboard.board[0].position).toStrictEqual([[0,1], [1,1]])
-})
+});
+
+test('Gameboard will not add a ship if any part of it is next to a ship already placed', () => {
+    gameboard.placeShip(2, "horizontal", 4, 4);
+});
