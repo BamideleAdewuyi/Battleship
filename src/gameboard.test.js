46309933df4269a2acf66c4dd006d71bea4494cb
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-08-01 17:21:21 +0100

    gameboard.test.js: Wrote test to check if gameboard can place a ship of length 2 at 0,1. Passed

--- src/gameboard.test.js
+++ src/gameboard.test.js
@@ -10,3 +10,7 @@ test('Can place a ship of length 2, horizontally  at 0,0', () => {
     gameboard.placeShip(2, "horizontal", 0, 0);
     expect(gameboard.board[0].position).toStrictEqual([[0,0], [1,0]])
 })
+
+test('Can place a ship of length 2, horizontally  at 0,1', () => {
+    expect(gameboard.board[0].position).toStrictEqual([[0,1], [1,1]])
+})
