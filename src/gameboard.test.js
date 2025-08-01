c92ce4f85f0483afbf6a6a52cb94173c40ba7a34
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-08-01 21:53:16 +0100

    gameboard.test.js: Third test now checks that checkSquareValid returns false for all squares for a ship on (4,4) and (5,4)

--- src/gameboard.test.js
+++ src/gameboard.test.js
@@ -18,7 +18,25 @@ test('Can place a ship of length 2, horizontally  at 0,1', () => {
 
 test('checkSquareValid method returns false with square next to placed ship', () => {
     gameboard.placeShip(2, 'horizontal', 4, 4);
-    expect(gameboard.checkSquareValid(4, 3)).toBe(false)
+    expect(gameboard.checkSquareValid(4, 4)).toBe(false);
+    expect(gameboard.checkSquareValid(4, 3)).toBe(false);
+    expect(gameboard.checkSquareValid(3, 3)).toBe(false);
+    expect(gameboard.checkSquareValid(3, 4)).toBe(false);
+    expect(gameboard.checkSquareValid(3, 5)).toBe(false);
+    expect(gameboard.checkSquareValid(4, 5)).toBe(false);
+    expect(gameboard.checkSquareValid(5, 5)).toBe(false);
+    expect(gameboard.checkSquareValid(5, 4)).toBe(false);
+    expect(gameboard.checkSquareValid(5, 3)).toBe(false);
+
+    expect(gameboard.checkSquareValid(5, 4)).toBe(false);
+    expect(gameboard.checkSquareValid(6, 4)).toBe(false);
+    expect(gameboard.checkSquareValid(6, 3)).toBe(false);
+    expect(gameboard.checkSquareValid(5, 3)).toBe(false);
+    expect(gameboard.checkSquareValid(4, 3)).toBe(false);
+    expect(gameboard.checkSquareValid(4, 4)).toBe(false);
+    expect(gameboard.checkSquareValid(4, 5)).toBe(false);
+    expect(gameboard.checkSquareValid(5, 5)).toBe(false);
+    expect(gameboard.checkSquareValid(6, 5)).toBe(false);
 })
 
 test('Gameboard will not add a ship if any part of it is next to a ship already placed', () => {
