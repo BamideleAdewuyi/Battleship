a53cdbadff1a40af50459d421f3e2831b59ce97a
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-08-02 18:25:19 +0100

    ship.test.js: Changed references to hits to damage

--- src/ship.test.js
+++ src/ship.test.js
@@ -8,7 +8,7 @@ beforeEach(() => {
 
 test('Ship class can register when its been hit', () => {
     ship.hit();
-    expect(ship.hits).toBe(1);
+    expect(ship.damage).toBe(1);
 });
 
 test('Ship is sunk after five hits', () => {
