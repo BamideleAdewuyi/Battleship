b89fc542bee5a5f3d6285d0afd3bb87318dddf96
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-08-03 01:38:51 +0100

    index.js: Imported style.css

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import GameController from "./Controllers/gameController";
 import GameView from "./Views/gameView";
 import Player from "./Models/player";
+import "./style.css";
 
 const game = new GameController(new GameView, new Player, new Player);
