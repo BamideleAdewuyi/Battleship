214c00caf4e23971e7f9314dc0d4bbbbd4c7294a
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-08-03 16:13:49 +0100

    gameView.js: Created gameArea with sides for each player

--- src/Views/gameView.js
+++ src/Views/gameView.js
@@ -7,7 +7,12 @@ class GameView {
         this.heading.textContent = "Battleship";
         this.headingDiv.append(this.heading);
 
-        this.container.append(this.headingDiv);
+        this.gameArea = document.createElement("div");
+        this.player1Side = document.createElement("div");
+        this.player2Side = document.createElement("div");
+        this.gameArea.append(this.player1Side, this.player2Side);
+
+        this.container.append(this.headingDiv, this.gameArea);
     };
 };
 
