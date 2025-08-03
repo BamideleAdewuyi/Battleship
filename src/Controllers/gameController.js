ddc049a295ccfb60701746ca60576f7148ec6ce9
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-08-03 01:23:12 +0100

    gameController.js: Created and exported GameController class

---
+++ src/Controllers/gameController.js
@@ -0,0 +1,9 @@
+class GameController {
+    constructor(view, player1, player2) {
+        this.view = view;
+        this.player1 = player1;
+        this.player2 = player2;
+    };
+};
+
+export default GameController;
