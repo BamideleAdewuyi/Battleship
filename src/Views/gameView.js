10e0a094d2b3bf9e8b295b546204d2f744a96c9e
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-08-03 16:17:57 +0100

    gameView.js: Gave elements classes and ids where necessary

--- src/Views/gameView.js
+++ src/Views/gameView.js
@@ -3,13 +3,20 @@ class GameView {
         this.container = document.querySelector(".container");
 
         this.headingDiv = document.createElement("div");
+        this.headingDiv.classList.add("headingDiv");
         this.heading = document.createElement("h2");
+        this.heading.classList.add("heading")
         this.heading.textContent = "Battleship";
         this.headingDiv.append(this.heading);
 
         this.gameArea = document.createElement("div");
+        this.gameArea.classList.add("gameArea");
         this.player1Side = document.createElement("div");
+        this.player1Side.classList.add("playerSide");
+        this.player1Side.setAttribute("id", "player1");
         this.player2Side = document.createElement("div");
+        this.player2Side.classList.add("playerSide");
+        this.player2Side.setAttribute("id", "player2");
         this.gameArea.append(this.player1Side, this.player2Side);
 
         this.container.append(this.headingDiv, this.gameArea);
