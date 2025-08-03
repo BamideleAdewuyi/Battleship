2f536c73702f36a6bb9ab42f8b1d4967ab75377a
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-08-03 15:06:17 +0100

    gameView.js: Added heading to constructor

--- src/Views/gameView.js
+++ src/Views/gameView.js
@@ -1,6 +1,13 @@
 class GameView {
     constructor() {
         this.container = document.querySelector(".container");
+
+        this.headingDiv = document.createElement("div");
+        this.heading = document.createElement("h2");
+        this.heading.textContent = "Battleship";
+        this.headingDiv.append(this.heading);
+
+        this.container.append(this.headingDiv);
     };
 };
 
