b6a629e985fff440cd76359961c8e42eab403401
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-08-08 12:21:29 +0100

    player.js: If player is computer, it now initialises an array of all possible places to attack. Also has removeItem method

--- src/Models/player.js
+++ src/Models/player.js
@@ -4,7 +4,26 @@ class Player {
     constructor(type) {
         this.type = type;
         this.gameboard = new Gameboard();
+        if (type == "Computer") {
+            this.squares = []
+            this.initialise();
+        }
     }
+    initialise() {
+        for (let x = 0; x < 11; x++) {
+            for (let y = 0; y < 11; y++) {
+                this.squares.push([x, y])
+            }
+        }
+    };
+
+    removeItem(arr, item) {
+        const index = arr.indexOf(item);
+        if (index > -1) {
+            arr.splice(index, 1);
+        }
+        return arr;
+    };
 };
 
 export default Player;
