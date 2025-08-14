387f0ac3a9105c0c4f55bede1331a88801ee0b2b
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-08-14 12:46:14 +0100

    gameController.js: handleNewGameButton now randomises player 2 ships

--- src/Controllers/gameController.js
+++ src/Controllers/gameController.js
@@ -13,17 +13,19 @@ class GameController {
         this.player2.initialise();
         this.view.renderGameStateHeading("It's your turn");
 
+        
         this.player1.gameboard.placeShip("Carrier", 5, "horizontal", 1, 10);
         this.player1.gameboard.placeShip("BattleShip", 4, "horizontal", 7, 10);
         this.player1.gameboard.placeShip("Cruiser", 3, "horizontal", 6, 8);
         this.player1.gameboard.placeShip("Submarine", 3, "vertical", 10, 3);
         this.player1.gameboard.placeShip("Destroyer", 2, "vertical", 6, 4);
         
-        this.player2.gameboard.placeShip("Carrier", 5, "horizontal", 1, 10);
-        this.player2.gameboard.placeShip("BattleShip", 4, "horizontal", 7, 10);
-        this.player2.gameboard.placeShip("Cruiser", 3, "horizontal", 6, 8);
-        this.player2.gameboard.placeShip("Submarine", 3, "vertical", 10, 3);
-        this.player2.gameboard.placeShip("Destroyer", 2, "vertical", 6, 4);
+        this.player2.gameboard.placeShipRandom("Carrier", 5);
+        this.player2.gameboard.placeShipRandom("BattleShip", 4);
+        this.player2.gameboard.placeShipRandom("Cruiser", 3);
+        this.player2.gameboard.placeShipRandom("Submarine", 3);
+        this.player2.gameboard.placeShipRandom("Destroyer", 2);
+
         this.view.renderBoardAndStats(this.view.player1Container, this.player1.gameboard, "Human", "1");
         this.view.renderBoardAndStats(this.view.player2Container, this.player2.gameboard, "Computer", "2");
         this.view.bindSquares(this.handleSquares);
